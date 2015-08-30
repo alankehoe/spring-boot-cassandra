b6d0de760e1bfb1709143fe1abb81964baf9a849
Alan Kehoe <alankehoe111@gmail.com>
2015-08-30 21:39:09 +0100

    cleanup

--- src/main/java/com/alankehoe/persistence/services/EventService.java
+++ src/main/java/com/alankehoe/persistence/services/EventService.java
@@ -2,14 +2,10 @@ package com.alankehoe.persistence.services;
 
 import com.alankehoe.persistence.mappers.EventColumnMapper;
 import com.alankehoe.persistence.models.Event;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.netflix.astyanax.Cluster;
-import com.netflix.astyanax.connectionpool.exceptions.ConnectionException;
 import com.netflix.astyanax.model.ColumnFamily;
 import com.netflix.astyanax.serializers.StringSerializer;
 import com.netflix.astyanax.serializers.UUIDSerializer;
 
-import java.util.List;
 import java.util.UUID;
 
 public class EventService extends BaseService<Event> {
