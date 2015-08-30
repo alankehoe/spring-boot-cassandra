3aa87297b2d39161e160d2838229360f82dfa8a1
Alan Kehoe <alankehoe111@gmail.com>
2015-08-30 09:57:10 +0100

    refactor

--- src/main/java/com/alankehoe/persistence/mappers/ColumnMapper.java
+++ src/main/java/com/alankehoe/persistence/mappers/ColumnMapper.java
@@ -7,30 +7,35 @@ import org.joda.time.DateTime;
 
 public abstract class ColumnMapper<T extends Entity> {
 
+    public static final String COLUMN_REF = "ref";
+    public static final String COLUMN_CREATED_AT = "created_at";
+    public static final String COLUMN_UPDATED_AT = "updated_at";
+
     protected Class<T> genericSuperClass = null;
-    
+
     public ColumnMapper(Class<T> genericSuperClass) {
         this.genericSuperClass = genericSuperClass;
     }
-    
+
     protected abstract void insertColumns(ColumnListMutation<String> column, T entity);
+
     protected abstract T convertColumns(ColumnList<String> columns, T entity);
 
     public void insert(ColumnListMutation<String> column, T entity) {
-        column.putColumn("ref", entity.getRef());
-        column.putColumn("created_at", entity.getCreatedAt().getMillis());
-        column.putColumn("updated_at", entity.getUpdatedAt().getMillis());
+        column.putColumn(COLUMN_REF, entity.getRef());
+        column.putColumn(COLUMN_CREATED_AT, entity.getCreatedAt().getMillis());
+        column.putColumn(COLUMN_UPDATED_AT, entity.getUpdatedAt().getMillis());
 
         insertColumns(column, entity);
     }
-    
+
     public T convert(ColumnList<String> columns) {
         T entity = getEmptyEntity();
-        
-        entity.setRef(columns.getColumnByName("ref").getUUIDValue());
-        entity.setCreatedAt(new DateTime(columns.getColumnByName("created_at").getLongValue()));
-        entity.setUpdatedAt(new DateTime(columns.getColumnByName("updated_at").getLongValue()));
-        
+
+        entity.setRef(columns.getColumnByName(COLUMN_REF).getUUIDValue());
+        entity.setCreatedAt(new DateTime(columns.getColumnByName(COLUMN_CREATED_AT).getLongValue()));
+        entity.setUpdatedAt(new DateTime(columns.getColumnByName(COLUMN_UPDATED_AT).getLongValue()));
+
         return convertColumns(columns, entity);
     }
 
--- src/main/java/com/alankehoe/persistence/mappers/UserColumnMapper.java
+++ src/main/java/com/alankehoe/persistence/mappers/UserColumnMapper.java
@@ -5,23 +5,27 @@ import com.netflix.astyanax.ColumnListMutation;
 import com.netflix.astyanax.model.ColumnList;
 
 public class UserColumnMapper extends ColumnMapper<User> {
-    
+
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_PASSWORD = "password";
+
     public UserColumnMapper() {
         super(User.class);
     }
     
     @Override
     protected void insertColumns(ColumnListMutation<String> column, User entity) {
-        column.putColumn("name", entity.getName());
-        column.putColumn("email", entity.getEmail());
-        column.putColumn("password", entity.getPassword());
+        column.putColumn(COLUMN_NAME, entity.getName());
+        column.putColumn(COLUMN_EMAIL, entity.getEmail());
+        column.putColumn(COLUMN_PASSWORD, entity.getPassword());
     }
 
     @Override
     protected User convertColumns(ColumnList<String> columns, User entity) {
-        entity.setName(columns.getColumnByName("name").getStringValue());
-        entity.setEmail(columns.getColumnByName("email").getStringValue());
-        entity.setPassword(columns.getColumnByName("password").getStringValue());
+        entity.setName(columns.getColumnByName(COLUMN_NAME).getStringValue());
+        entity.setEmail(columns.getColumnByName(COLUMN_EMAIL).getStringValue());
+        entity.setPassword(columns.getColumnByName(COLUMN_PASSWORD).getStringValue());
         
         return entity;
     }
