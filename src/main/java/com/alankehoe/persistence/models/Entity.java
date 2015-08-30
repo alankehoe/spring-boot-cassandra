1704d472ce73d8b6856fa5fcfe16ac168acb3a65
Alan Kehoe <alankehoe111@gmail.com>
2015-08-30 14:03:39 +0100

    equals and hashcodes

--- src/main/java/com/alankehoe/persistence/models/Entity.java
+++ src/main/java/com/alankehoe/persistence/models/Entity.java
@@ -1,5 +1,7 @@
 package com.alankehoe.persistence.models;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.joda.time.DateTime;
 
 import java.util.UUID;
@@ -32,5 +34,29 @@ public abstract class Entity {
 
     public void setUpdatedAt(DateTime updatedAt) {
         this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Entity)) return false;
+
+        Entity entity = (Entity) o;
+
+        return new EqualsBuilder()
+                .append(ref, entity.ref)
+                .append(createdAt, entity.createdAt)
+                .append(updatedAt, entity.updatedAt)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(ref)
+                .append(createdAt)
+                .append(updatedAt)
+                .toHashCode();
     }
 }
--- src/main/java/com/alankehoe/persistence/models/Event.java
+++ src/main/java/com/alankehoe/persistence/models/Event.java
@@ -1,5 +1,8 @@
 package com.alankehoe.persistence.models;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 public class Event extends Entity {
     
     private String payload;
@@ -10,5 +13,27 @@ public class Event extends Entity {
 
     public void setPayload(String payload) {
         this.payload = payload;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Event)) return false;
+
+        Event event = (Event) o;
+
+        return new EqualsBuilder()
+                .appendSuper(super.equals(o))
+                .append(payload, event.payload)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .appendSuper(super.hashCode())
+                .append(payload)
+                .toHashCode();
     }
 }
